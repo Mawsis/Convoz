3d1b262c093a95402b53720ded19bfc0e5f23b38
Mawsis <Mawsis448@outlook.com>
2024-03-20 00:53:31 +0100

    Quick fix

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get("/isLogged", function(Request $request){
+    $name = $request->user()?->name  ?? null;
+    return $name
+    ? response()->json(["message"=>"is logged"],202) 
+    : response()->json(["message"=>"is not logged"],203);
+});
+
+Route::get("/message/{to}"); 
